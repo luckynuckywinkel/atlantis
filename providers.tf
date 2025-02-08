339b98d2b7573bb452e6d18c54b5534b97899dd8
root <root@terrhost>
2025-02-08 10:54:57 +0000

    added providers

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 1.4"
+
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = "ru-central1-b"
+}
